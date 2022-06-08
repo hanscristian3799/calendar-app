f7113e460100b7cd8587417a8979ad9fd1925f68
hanscristian3799 <hanscristian376@gmail.com>
2022-06-08 20:58:40 +0700

    fix style

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -18,26 +18,26 @@ const Calendar = () => {
   };
 
   return (
-    <div className="card my-3">
+    <div className="my-3 me-5">
       <div className="calendar d-flex flex-column align-items-center justify-content-center">
-      <div className="calendar-header d-flex align-items-center justify-content-center">
-        <h2>
-          {months[currentDay.getMonth()]} {currentDay.getFullYear()}
-        </h2>
-      </div>
-      <div className="calendar-body d-flex flex-column flex-grow-1">
-        <div className="table-header d-flex align-items-end justify-content-between mx-5 fw-bold">
-          {days.map((d, index) => {
-            return (
-              <div className="weekday text-center" key={index}>
-                <p>{d}</p>
-              </div>
-            );
-          })}
+        <div className="calendar-header d-flex align-items-center justify-content-center">
+          <h2>
+            {months[currentDay.getMonth()]} {currentDay.getFullYear()}
+          </h2>
         </div>
-        <Days propsDay={currentDay} changeCurrentDay={changeCurrentDay} />
+        <div className="calendar-body d-flex flex-column flex-grow-1">
+          <div className="table-header d-flex align-items-end justify-content-between mx-5 fw-bold">
+            {days.map((d, index) => {
+              return (
+                <div className="weekday text-center" key={index}>
+                  <p>{d}</p>
+                </div>
+              );
+            })}
+          </div>
+          <Days propsDay={currentDay} changeCurrentDay={changeCurrentDay} />
+        </div>
       </div>
-    </div>
     </div>
   );
 };
